4e500a750136197160fa0f63e976b965c0e03e90
Jhoss Web <jhossweb@gmail.com>
2024-08-08 00:11:06 -0400

    creacion de controladores

---
+++ app/Http/Controllers/Vehiculos/TipoVehiculoController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Vehiculos;
+
+use App\Http\Controllers\Controller;
+use App\Models\TipoVehiculo;
+use Illuminate\Http\Request;
+
+class TipoVehiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoVehiculo $tipoVehiculo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipoVehiculo $tipoVehiculo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, TipoVehiculo $tipoVehiculo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoVehiculo $tipoVehiculo)
+    {
+        //
+    }
+}
